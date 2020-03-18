c930543e26ed7c8f1f97eb8e55de34a65f446e21
Sylvie <sylvie.z.lu@gmail.com>
2020-03-18 17:48:16 +0100

    dark light save local

--- toggle.js
+++ toggle.js
@@ -1,12 +1,22 @@
-var checkbox = document.querySelector('input[name=theme]');
+const checkbox = document.querySelector('input[name=theme]');
+const currentTheme = localStorage.getItem('data-theme');
 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        checkbox.checked = true;
+    }
+}
         checkbox.addEventListener('change', function() {
             if(this.checked) {
                 trans()
                 document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('data-theme', 'light');
             } else {
                 trans()
                 document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('data-theme', 'dark');
             }
         })
 
